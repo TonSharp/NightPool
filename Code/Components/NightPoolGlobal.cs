921459134ba0d40efc6b46f2cb759c5a45b44cb5
Anton Labokha <anton.labokha@cleverside.com>
2024-03-09 16:07:46 +0300

    Добавлена поддержка нескольких PoolPresets

--- Code/Components/NightPoolGlobal.cs
+++ Code/Components/NightPoolGlobal.cs
@@ -5,7 +5,9 @@
 // ----------------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace NTC.Pool
 {
@@ -16,163 +18,148 @@ namespace NTC.Pool
 #endif
     public sealed class NightPoolGlobal : MonoBehaviour
     {
-        [Header("Main")] 
-        [Tooltip(Constants.Tooltips.GlobalUpdateType)]
-        [SerializeField] private UpdateType _updateType = UpdateType.Update;
-        
-        [Header("Preload Pools")]
-        [Tooltip(Constants.Tooltips.GlobalPreloadType)]
-        [SerializeField] private PreloadType preloadPoolsType = PreloadType.Disabled;
-        
-        [Tooltip(Constants.Tooltips.PoolsToPreload)]
-        [SerializeField] private PoolsPreset poolsPreset;
+        [Header("Main")] [Tooltip(Constants.Tooltips.GlobalUpdateType)] [SerializeField]
+        private UpdateType _updateType = UpdateType.Update;
 
-        [Header("Global Pool Settings")] 
-        [Tooltip(Constants.Tooltips.OverflowBehaviour)]
-        [SerializeField] internal BehaviourOnCapacityReached _behaviourOnCapacityReached = Constants.DefaultBehaviourOnCapacityReached;
-        
-        [Tooltip(Constants.Tooltips.DespawnType)]
-        [SerializeField] internal DespawnType _despawnType = Constants.DefaultDespawnType;
-        
-        [Tooltip(Constants.Tooltips.CallbacksType)]
-        [SerializeField] internal CallbacksType _callbacksType = Constants.DefaultCallbacksType;
-        
-        [Tooltip(Constants.Tooltips.Capacity)]
-        [SerializeField, Min(0)] internal int _capacity = 64;
-        
-        [Tooltip(Constants.Tooltips.Persistent)]
-        [SerializeField] internal bool _dontDestroyOnLoad;
+        [FormerlySerializedAs("preloadPoolsType")] [Header("Preload Pools")] [Tooltip(Constants.Tooltips.GlobalPreloadType)] [SerializeField]
+        private PreloadType _preloadPoolsType = PreloadType.Disabled;
 
-        [Tooltip(Constants.Tooltips.Warnings)]
-        [SerializeField] internal bool _sendWarnings = true;
-        
-        [Header("Safety")] 
-        [Tooltip(Constants.Tooltips.NightPoolMode)]
-        [SerializeField] internal NightPoolMode _nightPoolMode = Constants.DefaultNightPoolMode;
-        
-        [Tooltip(Constants.Tooltips.DelayedDespawnReaction)]
-        [SerializeField] internal ReactionOnRepeatedDelayedDespawn _reactionOnRepeatedDelayedDespawn = Constants.DefaultDelayedDespawnHandleType;
-        
-        [Tooltip(Constants.Tooltips.DespawnPersistentClonesOnDestroy)]
-        [SerializeField] private bool _despawnPersistentClonesOnDestroy = true;
-        
-        [Tooltip(Constants.Tooltips.CheckClonesForNull)]
-        [SerializeField] private bool _checkClonesForNull = true;
-        
-        [Tooltip(Constants.Tooltips.CheckForPrefab)]
-        [SerializeField] private bool _checkForPrefab = true;
-        
-        [Tooltip(Constants.Tooltips.ClearEventsOnDestroy)]
-        [SerializeField] private bool _clearEventsOnDestroy;
+        [FormerlySerializedAs("poolsPreset")] [Tooltip(Constants.Tooltips.PoolsToPreload)] [SerializeField]
+        private List<PoolsPreset> _poolsPreset;
 
-#if UNITY_EDITOR
-        private void OnValidate()
-        {
-            if (Application.isPlaying)
-            {
-                NightPool.s_nightPoolMode = _nightPoolMode;
-                NightPool.s_checkForPrefab = _checkForPrefab;
-                NightPool.s_checkClonesForNull = _checkClonesForNull;
-                NightPool.s_despawnPersistentClonesOnDestroy = _despawnPersistentClonesOnDestroy;
-            }
-        }
-#endif
+        [Header("Global Pool Settings")] [Tooltip(Constants.Tooltips.OverflowBehaviour)] [SerializeField]
+        internal BehaviourOnCapacityReached _behaviourOnCapacityReached = Constants.BehaviourOnCapacityReached;
+
+        [Tooltip(Constants.Tooltips.DespawnTypeTooltip)] [SerializeField]
+        internal DespawnType _despawnType = Constants.DespawnType;
+
+        [Tooltip(Constants.Tooltips.CallbacksTypeTooltip)] [SerializeField]
+        internal CallbacksType _callbacksType = Constants.CallbacksType;
+
+        [Tooltip(Constants.Tooltips.Capacity)] [SerializeField] [Min(0)]
+        internal int _capacity = 64;
+
+        [Tooltip(Constants.Tooltips.Persistent)] [SerializeField]
+        internal bool _dontDestroyOnLoad;
+
+        [Tooltip(Constants.Tooltips.Warnings)] [SerializeField]
+        internal bool _sendWarnings = true;
+
+        [Header("Safety")] [Tooltip(Constants.Tooltips.NightPoolModeTooltip)] [SerializeField]
+        internal NightPoolMode _nightPoolMode = Constants.NightPoolMode;
+
+        [Tooltip(Constants.Tooltips.DelayedDespawnReaction)] [SerializeField]
+        internal ReactionOnRepeatedDelayedDespawn _reactionOnRepeatedDelayedDespawn =
+            Constants.DelayedDespawnHandleType;
+
+        [Tooltip(Constants.Tooltips.DespawnPersistentClonesOnDestroy)] [SerializeField]
+        private bool _despawnPersistentClonesOnDestroy = true;
+
+        [Tooltip(Constants.Tooltips.CheckClonesForNull)] [SerializeField]
+        private bool _checkClonesForNull = true;
+
+        [Tooltip(Constants.Tooltips.CheckForPrefab)] [SerializeField]
+        private bool _checkForPrefab = true;
+
+        [Tooltip(Constants.Tooltips.ClearEventsOnDestroy)] [SerializeField]
+        private bool _clearEventsOnDestroy;
+
         private void Awake()
         {
             Initialize();
             PreloadPools(PreloadType.OnAwake);
         }
 
-        private void Start()
-        {
-            PreloadPools(PreloadType.OnStart);
-        }
+        private void Start() => PreloadPools(PreloadType.OnStart);
 
         private void Update()
         {
             if (_updateType == UpdateType.Update)
-            {
                 HandleDespawnRequests(Time.deltaTime);
-            }
         }
-        
+
         private void FixedUpdate()
         {
             if (_updateType == UpdateType.FixedUpdate)
-            {
                 HandleDespawnRequests(Time.fixedDeltaTime);
-            }
         }
-        
+
         private void LateUpdate()
         {
             if (_updateType == UpdateType.LateUpdate)
-            {
                 HandleDespawnRequests(Time.deltaTime);
-            }
         }
 
-        private void OnApplicationQuit()
-        {
-            NightPool.s_isApplicationQuitting = true;
-        }
-        
         private void OnDestroy()
         {
             NightPool.ResetPool();
 
-            if (_clearEventsOnDestroy || NightPool.s_isApplicationQuitting)
-            {
+            if (_clearEventsOnDestroy || NightPool.IsApplicationQuitting)
                 NightPool.GameObjectInstantiated.Clear();
-            }
         }
+
+        private void OnApplicationQuit() => NightPool.IsApplicationQuitting = true;
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (!Application.isPlaying)
+                return;
+            
+            NightPool.NightPoolMode = _nightPoolMode;
+            NightPool.CheckForPrefab = _checkForPrefab;
+            NightPool.CheckClonesForNull = _checkClonesForNull;
+            NightPool.DespawnPersistentClonesOnDestroy = _despawnPersistentClonesOnDestroy;
+        }
+#endif
 
         private void Initialize()
         {
 #if DEBUG
-            if (NightPool.s_instance != null && NightPool.s_instance != this)
-                throw new Exception($"The number of {nameof(NightPool)} instances in the scene is greater than one!"); 
-            
+            if (NightPool.Instance != null && NightPool.Instance != this)
+                throw new Exception($"The number of {nameof(NightPool)} instances in the scene is greater than one!");
+
             if (enabled == false)
                 Debug.LogWarning($"The <{nameof(NightPoolGlobal)}> instance is disabled! " +
                                  "Some functions may not work because of this!", this);
 #endif
-            NightPool.s_isApplicationQuitting = false;
-            NightPool.s_instance = this;
-            NightPool.s_hasTheNightPoolInitialized = true;
-            NightPool.s_nightPoolMode = _nightPoolMode;
-            NightPool.s_checkForPrefab = _checkForPrefab;
-            NightPool.s_checkClonesForNull = _checkClonesForNull;
-            NightPool.s_despawnPersistentClonesOnDestroy = _despawnPersistentClonesOnDestroy;
+            NightPool.IsApplicationQuitting = false;
+            NightPool.Instance = this;
+            NightPool.HasTheNightPoolInitialized = true;
+            NightPool.NightPoolMode = _nightPoolMode;
+            NightPool.CheckForPrefab = _checkForPrefab;
+            NightPool.CheckClonesForNull = _checkClonesForNull;
+            NightPool.DespawnPersistentClonesOnDestroy = _despawnPersistentClonesOnDestroy;
         }
 
         private void PreloadPools(PreloadType requiredType)
         {
-            if (requiredType != preloadPoolsType)
+            if (requiredType != _preloadPoolsType)
                 return;
-            
-            NightPool.InstallPools(poolsPreset);
+
+            foreach (var preset in _poolsPreset)
+                NightPool.InstallPools(preset);
         }
 
         private void HandleDespawnRequests(float deltaTime)
         {
-            for (int i = 0; i < NightPool.DespawnRequests._count; i++)
+            for (var i = 0; i < NightPool.DespawnRequests.Count; i++)
             {
-                ref DespawnRequest request = ref NightPool.DespawnRequests._components[i];
+                ref var request = ref NightPool.DespawnRequests.Components[i];
 
-                if (request.Poolable._status == PoolableStatus.Despawned)
+                if (request.Poolable.Status == PoolableStatus.Despawned)
                 {
                     NightPool.DespawnRequests.RemoveUnorderedAt(i);
                     continue;
                 }
-                
+
                 request.TimeToDespawn -= deltaTime;
+
+                if (!(request.TimeToDespawn <= 0f))
+                    continue;
                 
-                if (request.TimeToDespawn <= 0f)
-                {
-                    NightPool.DespawnImmediate(request.Poolable);
-                    NightPool.DespawnRequests.RemoveUnorderedAt(i);
-                }
+                NightPool.DespawnImmediate(request.Poolable);
+                NightPool.DespawnRequests.RemoveUnorderedAt(i);
             }
         }
     }
